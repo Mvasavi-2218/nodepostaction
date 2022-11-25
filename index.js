3d42d1d4e0d2ccc16350d84cf77d788e3a37c69e
Mvasavi-2218 <muthavarapuvasavi94@gmail.com>
2022-11-25 12:00:55 +0530

    create registration page

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ app.get("/stu",(req,res)=>{
 })
 app.post("/newData",(req,res)=>{
 	//console.log(req.body)
-	const {name,age}=req.body
-	console.log(name,age)
+	const {name,age,course,mobile,email,year,gender}=req.body
+	console.log(name,age,course,mobile,email,year,gender)
 	res.send("Data Added")
 })
 app.listen(port,()=>console.log("Server starts at port 2003"))
+/*const express=require("exprees")
+const app=express()
+const port=2003
+const bodyParser=require("body-parser")*/
